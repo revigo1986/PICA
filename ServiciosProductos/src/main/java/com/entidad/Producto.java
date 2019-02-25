2bc713c8a5e7c51e2942ae8eb1b3f723369ebc43
revigo1986 <44505692+revigo1986@users.noreply.github.com>
2019-02-25 16:08:07 -0500

    Added good files
    Added good files for well-consumed WS.

--- ServiciosProductos/src/main/java/com/entidad/Producto.java
+++ ServiciosProductos/src/main/java/com/entidad/Producto.java
@@ -5,30 +5,33 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 //@JsonProperty for making match with the exact name in json response
 public class Producto {
-	private int codigo;
-	private String nombre;
+	private String type;
+    private Value value;
 
-	public int getCodigo() {
-		return codigo;
-	}
+    public Producto() {
+    }
 
-	public void setCodigo(int codigo) {
-		this.codigo = codigo;
-	}
+    public String getType() {
+        return type;
+    }
 
-	public String getNombre() {
-		return nombre;
-	}
+    public void setType(String type) {
+        this.type = type;
+    }
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	
-//    @Override
-//    public String toString() {
-//        return "Quote{" +
-//                "type='" + type + '\'' +
-//                ", value=" + value +
-//                '}';
-//    }
+    public Value getValue() {
+        return value;
+    }
+
+    public void setValue(Value value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Quote{" +
+                "type='" + type + '\'' +
+                ", value=" + value +
+                '}';
+    }
 }
